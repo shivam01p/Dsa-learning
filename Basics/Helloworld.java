d819d5f24b6ec4766d3e150e8a263d5cb73ca67f
shivam01p <shivamparmar549@gmail.com>
2026-02-11 17:08:23 +0530

    Printing Hello World

--- Basics/Helloworld.java
+++ Basics/Helloworld.java
@@ -1,7 +1,9 @@
 public class Helloworld{
     public static void main (String args[]){
         
-        System.out.print("Hello World");
+        System.out.print("Hello World");    //No next line
+        System.out.println("Hello World");  //Next line after Hello World
+        System.out.print("Hello \n World"); //Next line after Hello 
 
     }
 }
