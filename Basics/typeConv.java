b3a213139d39f1df7b5177bedf885764cab4391b
shivam01p <shivamparmar549@gmail.com>
2026-02-11 18:56:04 +0530

    Type conversion and Type casting learned

---
+++ Basics/typeConv.java
@@ -0,0 +1,15 @@
+public class typeConv {
+    public static void main(String[] args) {
+        
+        // Type conversion, Implicit type conversion
+        char a = 'a';
+        char b = 'b';
+        int res = a + b;
+        System.out.println(res);
+
+
+        //Type casting, Explicit type conversion
+        char re = (char) res;
+        System.out.println(re);
+    }
+}
