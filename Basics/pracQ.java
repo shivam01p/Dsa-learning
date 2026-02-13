330195db56134dead639c9ffcf4390ae0598b19c
shivam01p <shivamparmar549@gmail.com>
2026-02-13 17:15:46 +0530

    Practice Question solved

---
+++ Basics/pracQ.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class pracQ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Q1 Find average
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+
+        // int avg = (a+b+c)/3;
+        // System.out.println(avg);
+
+        // Q2 Find Area of square
+
+        // int side = sc.nextInt();
+        // int aos = side*side;
+        // System.out.println(aos);
+
+        // Q3 Find total cost with gst 
+
+        // float pencil = sc.nextFloat();
+        // float pen = sc.nextFloat();
+        // float eraser = sc.nextFloat();
+
+        // float TC = ((pencil+pen+eraser)* 18)/100;
+        // System.out.println(TC);
+
+        // Q4 
+
+        // int $ = 24;
+        // System.out.println($);
+
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+
+        int exp = 4/3 * (x+34) + 9 * (a+b+c) + (3 + y * (2+a)) / (a + b * y);
+
+        System.out.println(exp);
+    }
+}
