0fc5f24e99b0e0b0d35a464d1919ba16b7234529
shivam01p <shivamparmar549@gmail.com>
2026-03-06 15:25:50 +0530

    BtoD and DtoB done

--- Basics/binConv.java
+++ Basics/binConv.java
@@ -2,8 +2,28 @@ import java.util.*;
 
 public class binConv {
 
+    public static void BtoD(int binNum){
+
+        int mynum = binNum;
+        int decNum = 0;
+        int lastDigit;
+        int pow = 0; 
+
+        while(binNum > 0){
+
+            lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+
+            binNum /= 10;
+            pow++;
+        }
+
+        System.out.println("The Decimal num of " + mynum + " is: " + decNum);
+    }
+
     public static void DtoB(int decNum){
 
+        int mynum = decNum;
         int rem;
         int binNum = 0;
         int pow = 0;
@@ -17,17 +37,13 @@ public class binConv {
             pow++;
         }
 
-        System.out.println("The binary num is: "+ binNum);
+        System.out.println("The binary num of "+ mynum +  " is: "+ binNum);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        BtoD(1000);
         DtoB(8);
-
-
-        for(int i =0; i<8; i++){
-
-        }
         
     }
 }
